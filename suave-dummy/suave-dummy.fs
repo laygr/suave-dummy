f383336606fbd8888e1c9e10083d2700a5b14beb
Jesús Irais González Romero <laygr@outlook.com>
2016-11-24 13:07:21 -0600

    use port from arg

--- suave-dummy/suave-dummy.fs
+++ suave-dummy/suave-dummy.fs
@@ -2,4 +2,16 @@ open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 
-startWebServer defaultConfig (OK "Hello World!")
+
+[<EntryPoint>]
+let main args =
+  let port =  System.UInt16.Parse args.[0]
+  let ip = System.Net.IPAddress.Parse "127.0.0.1"
+  let serverConfig =
+    { Web.defaultConfig with
+        homeFolder = Some __SOURCE_DIRECTORY__
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Warn
+        bindings = [ HttpBinding.mk HTTP ip port ] }
+
+  startWebServer serverConfig (OK "Hello World!")
+  0
