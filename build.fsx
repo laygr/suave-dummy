bb6e5e29c0ecdcec39d6d53b3027774a7bf3e076
Jesús Irais González Romero <laygr@outlook.com>
2016-11-24 12:08:39 -0600

    initial

---
+++ build.fsx
@@ -0,0 +1,41 @@
+// include Fake libs
+#r "./packages/FAKE/tools/FakeLib.dll"
+#r @"FakeLib.dll"
+
+open Fake
+open Fake.Azure
+open System
+open System.IO
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let solutionFile = "splxi.fsproj"
+
+Target "StageWebsiteAssets" (fun _ ->
+    let blacklist =
+        [ "typings"
+          ".fs"
+          ".config"
+          ".references"
+          "tsconfig.json" ]
+    let shouldInclude (file:string) =
+        blacklist
+        |> Seq.forall(not << file.Contains)
+    Kudu.stageFolder (Path.GetFullPath @"suave-dummy\WebHost") shouldInclude)
+
+Target "BuildSolution" (fun _ ->
+    solutionFile
+    |> MSBuildHelper.build (fun defaults ->
+        { defaults with
+            Verbosity = Some Minimal
+            Targets = [ "Build" ]
+            Properties = [ "Configuration", "Release"
+                           "OutputPath", Kudu.deploymentTemp ] })
+    |> ignore)
+
+Target "Deploy" Kudu.kuduSync
+
+"StageWebsiteAssets"
+==> "BuildSolution"
+==> "Deploy"
+
+RunTargetOrDefault "Deploy"
